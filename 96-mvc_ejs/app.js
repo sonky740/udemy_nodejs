97226b362514189695cd952f44731667f8a14810
Sonky <sonky740@gmail.com>
2022-09-11 22:16:26 +0900

    98. 컨트롤러 추가하기 ~~

--- 96-mvc_ejs/app.js
+++ 96-mvc_ejs/app.js
@@ -4,20 +4,9 @@ const path = require('path');
 const express = require('express');
 const rootDir = require('./util/path');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// app.engine(
-//   'hbs',
-//   expressHbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs',
-//   })
-// );
-// app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -31,8 +20,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-
   res.status(404).render('404', { pageTitle: 'Page Not Found!' });
 });
 
