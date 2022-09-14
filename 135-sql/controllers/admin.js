37cfa58ac0114a2eb1c4956848021bff7a7544e8
Sonky <sonky740@gmail.com>
2022-09-14 13:54:11 +0900

    167. 카트 생성 및 가져오기 ~~

--- 135-sql/controllers/admin.js
+++ 135-sql/controllers/admin.js
@@ -13,12 +13,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title,
-    price,
-    imageUrl,
-    description,
-  })
+  req.user
+    .createProduct({ title, price, imageUrl, description })
     .then((result) => {
       console.log('Created Product');
       res.redirect('/admin/products');
@@ -32,8 +28,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    .then((products) => {
+      const product = products[0];
       if (!product) {
         return res.redirect('/');
       }
@@ -69,7 +67,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render('admin/products', {
         prods: products,
